0e250065baab7273d54b7057317af88a83222441
Brian Sunday <brians931@gmail.com>
2020-09-12 18:01:16 +0300

    add hero and banner to homepage

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import "./Landing.css";
+
+import Hero from "../../components/Hero/Hero";
+import Banner from "../../components/Banner/Banner";
+
+
+
 
 function Landing() {
     return (
-        <div>
-            <h2>Landinf works</h2>
-        </div>
+        <Hero>
+            <Banner title="We provide innovative software solutions & application development.">
+                <Link to="/" className="btn btn-outline-primary">
+                    wiss
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
 
