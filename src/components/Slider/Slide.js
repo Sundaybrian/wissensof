ecaa1146a18cc07c1547add6c241bb9782c984ac
Brian Sunday <brians931@gmail.com>
2020-09-14 15:17:58 +0300

    add slide component

---
+++ src/components/Slider/Slide.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { css, jsx } from "@emotion/core";
+
+function Slide({ content }) {
+  return (
+    <div
+      css={css`
+        height: 100;
+        width: 100%;
+        background-image: url("${content}");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+      `}
+    ></div>
+  );
+}
+
+export default Slide;
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,8 +1,26 @@
 import React, { useState } from "react";
 import { css, jsx } from "@emotion/core";
+import SliderContent from "./SliderContent";
 
 function Slider() {
-  return <div css={SliderCSS}></div>;
+  const getWidth = () => window.innerWidth;
+
+  const [state, setState] = useState({
+    translate: 0,
+    transition: 0.45,
+  });
+
+  const { translate, transition } = state;
+
+  return (
+    <div css={SliderCSS}>
+      <SliderContent
+        translate={translate}
+        transition={transition}
+        width={getWidth()}
+      ></SliderContent>
+    </div>
+  );
 }
 
 const SliderCSS = css`
