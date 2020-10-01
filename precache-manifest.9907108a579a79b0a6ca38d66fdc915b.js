73d467c2f57a2bdb46124807c50394b2c8e3884e
Brian Sunday <brians931@gmail.com>
2020-10-01 22:53:54 +0300

    Updates

---
+++ precache-manifest.9907108a579a79b0a6ca38d66fdc915b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ce8a0266ccfd1a6632239d53ad89d3",
+    "url": "/wissensof/index.html"
+  },
+  {
+    "revision": "b7071b954d74acd7b376",
+    "url": "/wissensof/static/css/2.ad676774.chunk.css"
+  },
+  {
+    "revision": "24fbdf011f0a5763ce79",
+    "url": "/wissensof/static/css/main.158d85cb.chunk.css"
+  },
+  {
+    "revision": "b7071b954d74acd7b376",
+    "url": "/wissensof/static/js/2.4c5be79b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wissensof/static/js/2.4c5be79b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24fbdf011f0a5763ce79",
+    "url": "/wissensof/static/js/main.633f24e9.chunk.js"
+  },
+  {
+    "revision": "2b95af065b98b4263579",
+    "url": "/wissensof/static/js/runtime-main.ad922079.js"
+  },
+  {
+    "revision": "48a061aaef28f0bfff8b3980b4a3041a",
+    "url": "/wissensof/static/media/club.48a061aa.png"
+  },
+  {
+    "revision": "1c2d942b78c3289f50ade4c9a8b68b69",
+    "url": "/wissensof/static/media/hero.1c2d942b.jpg"
+  },
+  {
+    "revision": "12ea87a766bf72c013403d893470ece7",
+    "url": "/wissensof/static/media/person.12ea87a7.png"
+  }
+]);
