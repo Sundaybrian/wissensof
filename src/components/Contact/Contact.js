686e72f902c0903ab121a4f1bc415a7ee968b62c
Brian Sunday <brians931@gmail.com>
2020-11-02 11:11:34 +0300

    add external links

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,33 +5,52 @@ import { Link } from "react-router-dom";
 import { FaInstagram, FaTwitter, FaFacebook, FaLinkedin } from "react-icons/fa";
 
 const Contact = () => {
-  return (
-    <>
-      <div className="contact container">
-        <Title title="Interested in working together?" />
+    return (
+        <>
+            <div className="contact container">
+                <Title title="Interested in working together?" />
 
-        <div className="contact__details">
-          <div className="contact__email">
-            <div className="social">
-              <h5>Email Us</h5>
-              <Link to="#!">hello@wissensof.com</Link>
-            </div>
-          </div>
+                <div className="contact__details">
+                    <div className="contact__email">
+                        <div className="social">
+                            <h5>Email Us</h5>
+                            <Link to="#!">hello@wissensof.com</Link>
+                        </div>
+                    </div>
 
-          <div className="contact__social">
-            <h5>Follow Us</h5>
-            <div className="social__fa">
-              <FaInstagram className="social__icons" />
-              <FaTwitter className="social__icons" />
-              <FaFacebook className="social__icons" />
-              <FaLinkedin className="social__icons" />
+                    <div className="contact__social">
+                        <h5>Follow Us</h5>
+                        <div className="social__fa">
+                            <a
+                                href="https://instagram.com/wissensof?igshid=iv8806t7u778"
+                                target="_blank"
+                                rel="noopener"
+                            >
+                                <FaInstagram className="social__icons" />
+                            </a>
+                            <a
+                                href="https://twitter.com/wissensof?s=09"
+                                target="_blank"
+                                rel="noopener"
+                            >
+                                <FaTwitter className="social__icons" />
+                            </a>
+                            <a
+                                href="https://m.facebook.com/Wissensof/"
+                                target="_blank"
+                                rel="noopener"
+                            >
+                                <FaFacebook className="social__icons" />
+                            </a>
+                            {/* <Link>
+                                <FaLinkedin className="social__icons" />
+                            </Link> */}
+                        </div>
+                    </div>
+                </div>
             </div>
-            {/* <Link to="#!">instagram</Link> */}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Contact;
