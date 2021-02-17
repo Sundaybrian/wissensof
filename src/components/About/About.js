169bf857c4661a1600e9ea64618cf13ec4320a40
Brian Sunday <brians931@gmail.com>
2021-02-17 17:13:32 +0300

    typop fix

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,33 +3,35 @@ import "./About.css";
 import Title from "../Title";
 
 function About() {
-  return (
-    <div className="container">
-      <section className="about">
-        <Title title="About Us" />
-        <div className="row">
-          <div className="col-md-6">
-            <p>
-              {" "}
-              Wissensof Technologies is a software development company that
-              provides innovative solutions to complex business problems. We
-              understand that every business is different, and there are no
-              cookie cutter solutions. We take satisfaction in understanding
-              your business in order to provide you with applications that solve
-              your most pressing challenges.
-            </p>
-          </div>
-          <div className="col-md-6">
-            <p>
-              Our team is committed to providing cutting edge technological
-              solutions that are scalable, secure, and easy to use across
-              devices.
-            </p>
-          </div>
+    return (
+        <div className="container">
+            <section className="about">
+                <Title title="About Us" />
+                <div className="row">
+                    <div className="col-md-6">
+                        <p>
+                            {" "}
+                            The Wissensof Company is a software development
+                            company that provides innovative solutions to
+                            complex business problems. We understand that every
+                            business is different, and there are no cookie
+                            cutter solutions. We take satisfaction in
+                            understanding your business in order to provide you
+                            with applications that solve your most pressing
+                            challenges.
+                        </p>
+                    </div>
+                    <div className="col-md-6">
+                        <p>
+                            Our team is committed to providing cutting edge
+                            technological solutions that are scalable, secure,
+                            and easy to use across devices.
+                        </p>
+                    </div>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
+    );
 }
 
 export default About;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return (
-    <div className="container-fluid footer">
-      <div className="footer__wiss">
-        <p>{new Date().getFullYear()} Wissensof technologies</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="container-fluid footer">
+            <div className="footer__wiss">
+                <p>{new Date().getFullYear()} The Wissensof Company</p>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
